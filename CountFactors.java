d2b0fe56745194500c9fc2a2ef71beffb39d9132
Fatos Morina <fatosi.morina@gmail.com>
2017-04-16 09:56:45 +0200

    Refactor CountFactors

--- CountFactors.java
+++ CountFactors.java
@@ -5,19 +5,18 @@
 // System.out.println("this is a debug message");
 
 class Solution {
-    public int solution(int N) {
-        // write your code in Java SE 8
-        int numFactors = 0;
-        for (int i=1; i<=(double)Math.sqrt(N); i++) {
-            
-            if (N%i==0 && i*i!=N) {
-                numFactors = numFactors+2;
-            }
-            else if (N%i==0 && i*i==N) {
-                numFactors = numFactors+1;
-            }
-        }
-        
-        return numFactors;
-    }
+	public int solution(int N) {
+		// write your code in Java SE 8
+		int numFactors = 0;
+		for (int i = 1; i <= (double) Math.sqrt(N); i++) {
+			if (N % i == 0) {
+				if (i * i != N) {
+					numFactors = numFactors + 2;
+				} else if (i * i == N) {
+					numFactors = numFactors + 1;
+				}
+			}
+		}
+		return numFactors;
+	}
 }
